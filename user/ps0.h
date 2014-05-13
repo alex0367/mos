75d4c6a7162c822ac8db105ad240dcbdaa4fd654
ejzheng <ejzheng@qti.qualcomm.com>
2014-05-13 15:12:47 +0800

    not successful switch to user

--- user/ps0.h
+++ user/ps0.h
@@ -1,6 +1,11 @@
 #ifndef _PS0_H_
 #define _PS0_H_
 
+/* EFLAGS Register. */
+#define FLAG_MBS  0x00000002    /* Must be set. */
+#define FLAG_IF   0x00000200    /* Interrupt Flag. */
+
+
 #define _USER \
     __attribute__((section(".user")))
 
