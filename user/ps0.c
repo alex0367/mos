4da8a04e4225a3f8376bbd3f54a29f3de4311835
Zheng Ender <crazyender@gmail.com>
2014-05-14 15:46:04 +0800

    ps0 switched to user mode

--- user/ps0.c
+++ user/ps0.c
@@ -27,7 +27,7 @@ static void user_setup_enviroment(unsigned int code_addr, unsigned int stack_add
 
 	mm_add_dynamic_map(code_addr, 
 					   (unsigned int)user_process - KERNEL_OFFSET, 
-					   PAGE_ENTRY_USER_CODE);
+					   PAGE_ENTRY_USER_DATA);
 	ps_record_dynamic_map(code_addr);
 
 	while (stack_addr < code_addr)
@@ -39,21 +39,10 @@ static void user_setup_enviroment(unsigned int code_addr, unsigned int stack_add
 }
 
 
+
 void user_first_process_run()
 {
-	intr_frame if_;
-
-	memset (&if_, 0, sizeof if_);
-	if_.cs = USER_CODE_SELECTOR ;
-	if_.gs = if_.fs = if_.es = if_.ds = if_.ss = USER_DATA_SELECTOR  ;
-	if_.eflags = FLAG_IF | FLAG_MBS;
-	if_.eip = 0x00804000;
-	if_.ebp = if_.esp = 0x00803f00;
-
 	user_setup_enviroment(0x00804000, 0x00800000);
-
-	ps_update_tss((unsigned int)&if_);
-
-	asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
-
+	extern switch_to_user_mode();
+	switch_to_user_mode();
 }
