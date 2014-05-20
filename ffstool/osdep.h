965c75eec409219529544b6a3710aea4024c9692
Zheng Ender <crazyender@gmail.com>
2014-05-21 02:45:46 +0800

    make ffstool runnable in macos

--- ffstool/osdep.h
+++ ffstool/osdep.h
@@ -5,12 +5,28 @@
 #ifdef WIN32
 #include <windows.h>
 #include <lib/list.h>
-
 typedef struct _semaphore
 {
 	HANDLE event;
 
 }semaphore;
+#elif MACOS
+#include <lib/list.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct _semaphore
+{
+	unsigned long long event;
+
+}semaphore;
+
+unsigned GetCurrentTime();
+#endif
+
+
 
 void sema_init(semaphore* lock, char * name, int init_state);
 
@@ -20,11 +36,16 @@ void sema_trigger(semaphore* lock);
 
 void sema_reset(semaphore* lock);
 
-void* _kmalloc(unsigned size);
-void _kfree(void* buf);
+void* _kmalloc(unsigned size, const char* f, int line);
+void _kfree(void* buf, const char* f, int line);
 
+#ifdef WIN32
 #define kmalloc(size) _kmalloc(size, __FUNCTION__, __LINE__)
 #define kfree(buf) _kfree(buf, __FUNCTION__, __LINE__)
+#elif MACOS
+#define kmalloc(size) _kmalloc(size, __func__, __LINE__)
+#define kfree(buf) _kfree(buf, __func__, __LINE__)
+#endif
 
 
 typedef struct _task_struct
@@ -41,9 +62,6 @@ void printk(char* msg, ...);
 
 #define vmalloc(page_count) malloc(page_count*4096)
 #define vmfree(addr, page_count) free(addr)
-#else
 
-
-#endif
 
 #endif
