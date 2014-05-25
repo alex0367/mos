dc614080e6febb9086cecaa20cab295f5aa430ad
Zheng Ender <crazyender@gmail.com>
2014-05-26 02:21:00 +0800

    not runable fork

--- user/run.c
+++ user/run.c
@@ -3,9 +3,25 @@
 void main()
 {
 	int ret;
-    int pid = getpid();
-	char msg[] = "ps[0]: hello, world from user land\n";
-    msg[3] = pid+'0';
+	int pid = -1;
+	int psid = -1;
+	char msg[] = "ps[9]: hello, world from user land\n";
+	// msg[3] = pid+'0';
+	psid = fork();
+	write(1, "return\n",7);
+	while(1); 
+	if (psid){ // parent
+		while(1)
+			write(1, "parent\n", 7);
+	}else{
+		while(1)
+			write(1, "child \n", 7);
+	}
+
+	//while(1);
+	
+	pid = getpid();
+	msg[3] = pid+'0';
 	while(1)
 		write(1, msg, sizeof(msg));
 }
