7e3b8740da495e7e9f4898f6ed04e635e07a70a9
Ender Zheng <crazyender@gmail.com>
2014-05-26 15:08:05 +0800

    execve done

--- user/ps0.c
+++ user/ps0.c
@@ -6,16 +6,37 @@
 #include <mm/mm.h>
 #include <config.h>
 
+static void cleanup()
+{
+    task_struct* cur = CURRENT_TASK();
+    int i = 0;
 
+    for (i = 0; i < MAX_FD; i++) {
+        if (cur->fds[i] != 0 && 
+            cur->fds[i] != INODE_STD_IN &&
+            cur->fds[i] != INODE_STD_OUT &&
+            cur->fds[i] != INODE_STD_ERR) {
+            fs_close(cur->fds[i]);
+            cur->file_off[i] = 0;
+        }
+    }
 
-static void user_setup_enviroment()
+    ps_cleanup_all_user_map(cur);
+}
+
+int sys_execve(const char* file, char** argv, char** envp)
 {
     unsigned eip = 0;
     int i = 0;
     unsigned esp_buttom = KERNEL_OFFSET - USER_STACK_PAGES*PAGE_SIZE;
     unsigned esp_top = KERNEL_OFFSET - 4;
+    
+    char file_name[64] = {0};
+    strcpy(file_name, file);
 
-    eip = elf_map("/bin/run");
+    cleanup();
+
+    eip = elf_map(file_name);
     if (!eip) {
         printk("fatal error: no idle task found!\n");
         asm("hlt");
@@ -26,18 +47,23 @@ static void user_setup_enviroment()
     }
 
     extern void switch_to_user_mode(unsigned eip, unsigned esp);
-	switch_to_user_mode(eip, esp_top);
+    switch_to_user_mode(eip, esp_top);
+    // never return here
+    return 0;
+}
 
+static void user_setup_enviroment()
+{
+    unsigned int esp0;
+    __asm__("movl %%esp, %0" : "=r"(esp0));
+    ps_update_tss(esp0);
+    sys_execve("/bin/run", 0, 0);
 }
 
 
 
 void user_first_process_run()
 {
-	unsigned int esp0;
-    __asm__("movl %%esp, %0" : "=r"(esp0));
-	ps_update_tss(esp0);
-
-	user_setup_enviroment();
+    user_setup_enviroment();
 
 }
