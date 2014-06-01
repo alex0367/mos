b01286a662e1935c2fabb82611a351fb09785777
Zheng Ender <crazyender@gmail.com>
2014-06-01 23:17:29 +0800

    change user program into dynamic link

---
+++ user/crt0.c
@@ -0,0 +1,19 @@
+
+#include <syscall.h>
+#include <lib/klib.h>
+
+void start(int argc, char** argv, char** envp)
+{
+	unsigned status;
+
+	libc_init();
+
+    tty_init();
+
+
+	extern int main(int argc, char** argv);
+
+	status = main(argc, argv);
+
+	exit(status);
+}
