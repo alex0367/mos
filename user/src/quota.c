469ab372c9897e9df450742d0681e5493afcf6f5
Ender Zheng <crazyender@gmail.com>
2014-06-20 19:06:21 +0800

    add more logs

--- user/src/quota.c
+++ user/src/quota.c
@@ -7,8 +7,8 @@ int main()
 
 	quota(&q);
 
-	printf("kernel heap:\tcur %d, max %d\n", q.heap_cur, q.heap_wm);
-	printf("physical mm:\tcur %x, max %x, total %h\n", q.phymm_cur, q.phymm_wm, q.phymm_max);
+	printf("kernel heap:\tcur %d, max %d, top %x\n", q.heap_cur, q.heap_wm, q.heap_top);
+	printf("physical mm:\tcur %h, max %h, total %h\n", q.phymm_cur, q.phymm_wm, q.phymm_max);
 	return 0;
 }
 
