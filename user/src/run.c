136f0ea51d6424cf75e2c642c48719409453cf29
Zheng Ender <crazyender@gmail.com>
2014-06-26 01:05:19 +0800

    fix bugs in _task_sched

--- user/src/run.c
+++ user/src/run.c
@@ -1,16 +1,31 @@
 #include <syscall.h>
 
+static char* sh;
+static char* argv[2];
+
 void main()
 {
 	int i = 0;
 	int pid = -1;
 	int psid = -1;
+    struct stat s;
 
 	psid = fork();
 	if (psid){ // parent
 	
 	}else{ // child
-		execve("/bin/sh", 0, 0);		
+        if (stat("/bin/bash", &s) != -1) {
+            sh = "/bin/bash";
+            argv[0] = "/bin/bash";
+        }else if(stat("/bin/sh", &s) != -1) {
+            sh = "/bin/sh";
+            argv[1] = "/bin/sh";
+        }else{
+            printf("No sh!\n");
+            exit(-1);
+        }
+        argv[1] = 0;
+        execve(sh, argv, 0);		
 	}
 	
 	while(1){
@@ -20,7 +35,7 @@ void main()
 			printf("Fatal error: first process exit!\n");
 			psid = fork();
 			if(!psid){
-				execve("/bin/sh", 0, 0);
+				execve(sh, argv, 0);
 			}
 		}
         pause();
