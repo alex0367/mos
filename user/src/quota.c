09da62b826c38baf2b3141853bd6a4e95846c484
Zheng Ender <crazyender@gmail.com>
2014-07-01 16:20:43 +0800

    refine

--- user/src/quota.c
+++ user/src/quota.c
@@ -9,6 +9,7 @@ int main()
 
 	printf("kernel heap:\tcur %d, max %d, top %x\n", q.heap_cur, q.heap_wm, q.heap_top);
 	printf("physical mm:\tcur %h, max %h, total %h\n", q.phymm_cur, q.phymm_wm, q.phymm_max);
+	printf("page table cache:\ttop %x, count %d\n", q.pgc_top, q.pgc_count);
 	return 0;
 }
 
