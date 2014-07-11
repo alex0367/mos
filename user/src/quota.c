19c12ac73f615197ac1af4cf737c4747c6a35bed
Zheng Ender <crazyender@gmail.com>
2014-07-11 17:08:54 +0800

    speedup hdd cache by using rbtree
        write: if cache not exhaled, 800 k/s
        read:  if cache hot, 900 k/s
        almost twice as previous linear search cache

--- user/src/quota.c
+++ user/src/quota.c
@@ -7,7 +7,7 @@ int main()
 
 	quota(&q);
 
-	printf("kernel heap:\tcur %d, max %d, top %x\n", q.heap_cur, q.heap_wm, q.heap_top);
+	printf("kernel heap:\tcur %h, max %h, top %x\n", q.heap_cur, q.heap_wm, q.heap_top);
 	printf("physical mm:\tcur %h, max %h, total %h\n", q.phymm_cur, q.phymm_wm, q.phymm_max);
 	printf("page table cache:\ttop %x, count %d\n", q.pgc_top, q.pgc_count);
 	return 0;
