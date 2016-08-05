b31f2612e0c72ef5a07615497714216599707120
Zheng Ender <crazyender@gmail.com>
2016-08-05 17:12:57 +0800

    format code

--- user/src/ls.c
+++ user/src/ls.c
@@ -3,87 +3,94 @@
 
 static void print_mode(unsigned mode)
 {
-	char tmp[10] = {0};
-	char per[3] = {'x', 'r', 'w'};
-	int i = 10;
+    char tmp[10] = {0};
+    char per[3] = {'x', 'r', 'w'};
+    int i = 10;
 
-	if(S_ISDIR(mode))
-	  tmp[0] = 'd';
-	else
-	  tmp[0] = '-';
+    if (S_ISDIR(mode))
+        tmp[0] = 'd';
+    else
+        tmp[0] = '-';
 
-	for (i = 9; i > 0; i--){
-		if (mode & 0x00000001)
-		  tmp[i] = per[i % 3];
-		else
-		  tmp[i] = '-';
-		mode = mode >> 1;
-	}
-	printf("%s ", tmp);
+    for (i = 9; i > 0; i--)
+    {
+        if (mode & 0x00000001)
+            tmp[i] = per[i % 3];
+        else
+            tmp[i] = '-';
+        mode = mode >> 1;
+    }
+    printf("%s ", tmp);
 }
 
 static void print_links(struct stat* s)
 {
-	printf("1 ");
+    printf("1 ");
 }
 
 static void print_uname(struct stat* s)
 {
-	printf("root ");
+    printf("root ");
 }
 
 static void print_gname(struct stat* s)
 {
-	printf("root ");
+    printf("root ");
 }
 
 static void print_size(struct stat* s)
 {
-	printf("%h ", s->st_size);
+    printf("%h ", s->st_size);
 }
 
 static void print_time(struct stat* s)
 {
-	printf("%x ", s->st_mtime);
+    printf("%x ", s->st_mtime);
 }
 
 int main(int argc, char** argv)
 {
     // FIXME
-	// now can only list current
+    // now can only list current
     struct dirent ent;
     unsigned dir = 0;
-	char cwd[256] = {0};
+    char cwd[256] = {0};
 
-	getcwd(cwd, 256);
-	dir = open(cwd);
-    if (!dir) {
+    getcwd(cwd, 256);
+    dir = open(cwd);
+    if (!dir)
+    {
         return -1;
     }
 
-    while (readdir(dir, &ent)) {
-		char sub[256] = {0};
-		struct stat s;
-		if (!strcmp(cwd, "/")){
-			strcpy(sub, "/");
-			strcat(sub, ent.d_name);
-		}else{
-			strcpy(sub, cwd);
-			strcat(sub, "/");
-			strcat(sub, ent.d_name);
-		}
-		if (stat(sub, &s) == -1){
-			printf("Fatal error!\n");
-			break;
-		}
+    while (readdir(dir, &ent))
+    {
+        char sub[256] = {0};
+        struct stat s;
+        if (!strcmp(cwd, "/"))
+        {
+            strcpy(sub, "/");
+            strcat(sub, ent.d_name);
+        }
+        else
+        {
+            strcpy(sub, cwd);
+            strcat(sub, "/");
+            strcat(sub, ent.d_name);
+        }
+        if (stat(sub, &s) == -1)
+        {
+            printf("Fatal error!\n");
+            break;
+        }
 
         print_mode(ent.d_mode);
-		print_links(&s);
-		print_uname(&s);
-		print_gname(&s);
-		print_size(&s);
-		print_time(&s);
-		printf("%s\n", ent.d_name);
+        print_links(&s);
+        print_uname(&s);
+        print_gname(&s);
+        print_size(&s);
+        print_time(&s);
+        printf("%s\n", ent.d_name);
     }
 
     close(dir);
--- user/src/mkdir.c
+++ user/src/mkdir.c
@@ -7,40 +7,45 @@
 
 static int mkdir_file(const char* f)
 {
-	struct stat s;
-	char file[256] = {0};
-	
-	if (!f)
-	  return -1;
+    struct stat s;
+    char file[256] = {0};
 
-	if (f[0] != '/'){
-		getcwd(file, 256);
-		if( strcmp(file, "/") != 0 )
-		  strcat(file, "/");
-		strcat(file, f);
-	}else{
-		strcpy(file, f);
-	}
+    if (!f)
+        return -1;
 
-	if( stat(file, &s) != -1){
-		printf("File already exist\n");
-		return -1;
-	}
+    if (f[0] != '/')
+    {
+        getcwd(file, 256);
+        if (strcmp(file, "/") != 0)
+            strcat(file, "/");
+        strcat(file, f);
+    }
+    else
+    {
+        strcpy(file, f);
+    }
 
-	return mkdir(file, S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);
+    if (stat(file, &s) != -1)
+    {
+        printf("File already exist\n");
+        return -1;
+    }
+
+    return mkdir(file, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
 
 }
 
 int main(int argc, char** argv)
 {
-	if(argc < 2)
-	{
-		printf("Invalid parameter\n");
-		return -1;
-	}
-	else{
-		return mkdir_file(argv[1]);
-	}
+    if (argc < 2)
+    {
+        printf("Invalid parameter\n");
+        return -1;
+    }
+    else
+    {
+        return mkdir_file(argv[1]);
+    }
     return 0;
 }
